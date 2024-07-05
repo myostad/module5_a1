fc34321267018f0d3e597db20c8442c7abcf455d
Shahadat H <bdhostit@gmail.com>
2024-07-05 16:10:08 +0600

    Add files via upload

---
+++ celsius_to_fahrenheit.php
@@ -0,0 +1,5 @@
+<?php  
+    $celsius = 32 ;  
+    $fahrenheit = ($celsius*(9/5))+32 ;   //°F = (°C × (9/5)) + 32
+
+    printf("The temperature of %d degree Celsius is equivalent to %.2f degree Fahrenheit.", $celsius, $fahrenheit);
---
+++ largest_number.php
@@ -0,0 +1,13 @@
+<?php
+
+$num1 = 4;
+$num2 = 5;
+$num3 = 6;
+
+if ($num1>$num2) {
+    echo "The number $num1 is the largest number.";
+} elseif ($num2>$num3) {
+    echo "The number $num2 is the largest number.";
+} else {
+    echo "The number $num3 is the largest number.";
+}
